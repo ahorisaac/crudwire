cd5b184a6d92022271de9b73dbdd7d4db36ff8be
ahorisaac <ahorisaac.ai@gmail.com>
2024-04-20 02:44:54 +0100

    feat: create the create functionality

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class TodoList extends Component
 {
+    #[Rule('required|min:3|max:50')]
+    public $name;
+
+    public function create()
+    {
+        // validate the name of the todo
+        $validated = $this->validateOnly('name');
+
+        // create the todo
+        Todo::create($validated);
+
+        // clear the input box
+        $this->reset('name');
+
+        // send a flash message
+        session()->flash('success', 'Created.');
+    }
+
     public function render()
     {
         return view('livewire.todo-list');
