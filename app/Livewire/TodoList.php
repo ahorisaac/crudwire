b958f70b707b4c97dcb97ba22f385319820814da
ahorisaac <ahorisaac.ai@gmail.com>
2024-04-20 03:28:05 +0100

    feat: create the read & search functionality

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -5,11 +5,16 @@ namespace App\Livewire;
 use App\Models\Todo;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+
     #[Rule('required|min:3|max:50')]
     public $name;
+
+    public $search;
 
     public function create()
     {
@@ -28,6 +33,8 @@ class TodoList extends Component
 
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(5),
+        ]);
     }
 }
